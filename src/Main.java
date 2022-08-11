d62ded76f446d785483cccaafb02f910bd69c520
kwamezigi <102569865+kwamezigi@users.noreply.github.com>
2022-08-11 00:31:09 +0000

    Add files via upload
    class work-- a program to find the standard deviation of a given size of an array

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,56 @@
-// A code to select a female first class student in the university for a scholarship    by;(Abu Nana Kwame Isaac- 10974843)
+// A program to calculate for Population Standard Deviation
 
 public class Main {
     public static void main(String[] args) {
-        SelectGirl((float) 3.7, "female");
-        
+
+        int[] arr = {6, 5, 13, 8};
+        PopStanDev(arr);
+//        SampleStanDev(arr);
+
     }
-    
-     static void SelectGirl(float gpa, String gender) {
-        if (gpa >= 3.6) {
-            if (gender == "female") {
-                System.out.println("You qualify for this selection");
-            } else {
-                System.out.println("You don't qualify for this selection");
-            }
-        } else {
-            System.out.println("You don't qualify for this selection");
+    // Population Standard Deviation
+    static void PopStanDev(int[] arr) {
+        float sum = 0;
+        float count = 0; //this will hold the number of elements in the array
+
+        for (int i = 0; i < arr.length ; i++) {
+            sum += arr[i];
+            count++;
         }
+        float average = (sum / count); // calculates the average for the array
+        System.out.print("The Mean is " + average);
+
+        float sumNum = 0; // represents the numerator of the standard deviation formulae
+        for (int i = 0; i < arr.length; i++) {
+            float numb = (arr[i] - (average));
+            float number = (numb * numb); // calculates the square of (Xi-Mean)
+            sumNum =  (sumNum + number);
+        }
+
+        float ans = (float) (Math.sqrt((sumNum / count)));
+        System.out.printf(", and the population standard deviation is %.2f", ans);
+
+    }
+// Sample Standard Deviation
+    static void SampleStanDev(int[] arr) {
+        float sum = 0;
+        float count = 0; //this will hold the number of elements in the array
+
+        for (int i = 0; i < arr.length ; i++) {
+            sum += arr[i];
+            count++;
+        }
+        float average = (sum / count); // calculates the average for the array
+        System.out.print("The Mean is " + average);
+
+        float sumNum = 0; // represents the numerator of the standard deviation formulae
+        for (int i = 0; i < arr.length; i++) {
+            float numb = (arr[i] - (average));
+            float number = (numb * numb); // calculates the square of (Xi-Mean)
+            sumNum =  (sumNum + number);
+        }
+
+        float ans = (float) (Math.sqrt((sumNum / (count - 1))));
+        System.out.printf(", and the sample standard deviation is %.2f", ans);
     }
 }
